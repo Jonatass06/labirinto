edcc65b51d9af2539968c35cb98b0da9e5585e46
Jonatas Jackson Goncalves <jonatasg@weg.net>
2023-10-10 13:31:35 -0300

    correção dois pulos

--- components/quadrado.jsx
+++ components/quadrado.jsx
@@ -1,29 +1,65 @@
 import lista from "@/data/lista";
 import { useEffect, useState } from "react";
 
-export default function Quadrado(props){
+export default function Quadrado(props) {
 
-    const [isHere, setIsHere] = useState(props.isHere );
+    const [isHere, setIsHere] = useState(props.isHere);
+    const quadrados = lista;
 
     useEffect(() => {
-        document.body.addEventListener("keydown", (event) => {
-            if(lista[props.chave] == 2){
+        document.body.addEventListener("keydown", async (event) => {
+            let tecla = event.key;
+            let indice = quadrados.indexOf(2);
+
+            if (tecla == 'w' || tecla == 'W') {
+                if (indice - 23 >= 0) {
+                    if (quadrados[indice - 23] != 1) {
+                        quadrados[indice - 23] = 2;
+                        quadrados[indice] = 0;
+                    }
+                }
+            } else if (tecla == 'a' || tecla == 'A') {
+                if (quadrados[indice - 1] != 1) {
+                    quadrados[indice] = 0;
+                    quadrados[indice - 1] = 2;
+                }
+
+            } else if (tecla == 's' || tecla == 'S') {
+                if (indice + 23 < 506) {
+                    if (quadrados[indice + 23] != 1) {
+                        quadrados[indice + 23] = 2;
+                        quadrados[indice] = 0;
+                    }
+                }
+
+            } else if (tecla == 'd' || tecla == 'D') {
+                if (quadrados[indice + 1] != 1) {
+                    quadrados[indice + 1] = 2;
+                    quadrados[indice] = 0;
+                }
+                console.log(lista)
+            }
+            if (quadrados[482] == 2) {
+                console.log("oi 9vinha")
+            }
+
+            if (lista[props.chave] == 2) {
                 setIsHere(true);
-            } else{
+            } else {
                 setIsHere(false);
             }
             console.log(isHere)
         });
-      })
+    })
 
     let cor = props.parede ? "bg-black" : "bg-white";
-    return(
+    return (
         <>
-        <div className={"w-8 h-8 " + cor} >
-            {
-               isHere ? <img src="VASCO.png" alt="" /> : <></>
-            }
-        </div>
+            <div className={"w-8 h-8 " + cor} >
+                {
+                    isHere ? <img src="VASCO.png" alt="" /> : <></>
+                }
+            </div>
         </>
-     )
+    )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -7,46 +7,6 @@ export default function Home() {
   let i=-1;
   const quadrados = lista;
 
-
-  useEffect(() => {
-    document.body.addEventListener("keydown", (event) => {
-      let tecla = event.key;
-      let indice = quadrados.indexOf(2);
-
-      if (tecla == 'w' || tecla == 'W') {
-        if (indice - 23 >= 0) {
-          if (quadrados[indice - 23] != 1) {
-            quadrados[indice - 23] = 2;
-            quadrados[indice] = 0;
-          }
-        }
-      } else if (tecla == 'a' || tecla == 'A') {
-        if (quadrados[indice - 1] != 1) {
-          quadrados[indice] = 0;
-          quadrados[indice - 1] = 2;
-        }
-
-      } else if (tecla == 's' || tecla == 'S') {
-        if (indice + 23 < 506) {
-          if (quadrados[indice + 23] != 1) {
-            quadrados[indice + 23] = 2;
-            quadrados[indice] = 0;
-          }
-        }
-
-      } else if (tecla == 'd' || tecla == 'D') {
-        if (quadrados[indice + 1] != 1) {
-          quadrados[indice + 1] = 2;
-          quadrados[indice] = 0;
-        }
-        console.log(lista)
-      }
-      if (quadrados[482] == 2) {
-        console.log("oi 9vinha")
-      }
-    });
-  })
-
   return (
 
     <div className="w-screen h-screen flex justify-center items-center">
