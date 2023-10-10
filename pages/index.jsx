2b3824b2fc8714af36dac53a8b2f48f3d3a2391f
Jonatas Jackson Goncalves <jonatasg@weg.net>
2023-10-10 13:57:42 -0300

    correção dois pulos

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,6 @@
 import Quadrado from "@/components/quadrado"
 import lista from "@/data/lista";
 import { useEffect, useState } from "react";
-import enventoAlterarArray from "./api/atualizarArray";
 
 
 export default function Home() {
@@ -9,7 +8,42 @@ export default function Home() {
   const quadrados = lista; 
 
     useEffect(() => {
-        enventoAlterarArray();
+      document.body.addEventListener("keydown", (event) => {
+        let tecla = event.key;
+        let indice = quadrados.indexOf(2);
+
+        if (tecla == 'w' || tecla == 'W') {
+          if (indice - 23 >= 0) {
+            if (quadrados[indice - 23] != 1) {
+              quadrados[indice - 23] = 2;
+              quadrados[indice] = 0;
+            }
+          }
+        } else if (tecla == 'a' || tecla == 'A') {
+          if (quadrados[indice - 1] != 1) {
+            quadrados[indice] = 0;
+            quadrados[indice - 1] = 2;
+          }
+
+        } else if (tecla == 's' || tecla == 'S') {
+          if (indice + 23 < 506) {
+            if (quadrados[indice + 23] != 1) {
+              quadrados[indice + 23] = 2;
+              quadrados[indice] = 0;
+            }
+          }
+
+        } else if (tecla == 'd' || tecla == 'D') {
+          if (quadrados[indice + 1] != 1) {
+            quadrados[indice + 1] = 2;
+            quadrados[indice] = 0;
+          }
+          console.log(lista)
+        }
+        if (quadrados[482] == 2) {
+          console.log("oi 9vinha")
+        }
+      });
     })
 
   return (
@@ -20,11 +54,11 @@ export default function Home() {
         {quadrados.map(quadrado => {
           i++;
           if (quadrado == 1) {
-            return <Quadrado parede key={i} chave={i} />
+            return <Quadrado parede key={i} isHere={false} chave={i} />
           } else if (quadrado == 2) {
             return <Quadrado key={i} isHere chave={i} />
           }
-          return <Quadrado key={i} chave={i} />
+          return <Quadrado key={i} isHere={false} chave={i} />
         })}
 
       </div>
