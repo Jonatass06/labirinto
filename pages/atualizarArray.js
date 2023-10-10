1f97158c8c6697584180649b5289d4b79e16a03a
Jonatas Jackson Goncalves <jonatasg@weg.net>
2023-10-10 14:22:08 -0300

    Adicionar modal de vitória

---
+++ pages/atualizarArray.js
@@ -0,0 +1,42 @@
+import lista from "@/data/lista";
+
+export default function atualizarArray(){
+    document.body.addEventListener("keydown", (event) => {
+        const quadrados = lista;
+        // Tecla pressionada
+        let tecla = event.key;
+        // Indice do vasco no tabuleiro
+        let indice = quadrados.indexOf(2);
+  
+        // Andando para cima
+        if (tecla == 'w' || tecla == 'W' || tecla == "ArrowUp") {
+          if (indice - 23 >= 0) {
+            if (quadrados[indice - 23] != 1) {
+              quadrados[indice - 23] = 2;
+              quadrados[indice] = 0;
+            }
+          }
+          // Andando para a esquerda
+        } else if (tecla == 'a' || tecla == 'A' || tecla == "ArrowLeft") {
+          if (quadrados[indice - 1] != 1) {
+            quadrados[indice] = 0;
+            quadrados[indice - 1] = 2;
+          }
+          // Andando para baixo
+        } else if (tecla == 's' || tecla == 'S' || tecla == "ArrowDown") {
+          if (indice + 23 < 506) {
+            if (quadrados[indice + 23] != 1) {
+              quadrados[indice + 23] = 2;
+              quadrados[indice] = 0;
+            }
+          }
+          // Andando para a direita
+        } else if (tecla == 'd' || tecla == 'D' || tecla == "ArrowRight") {
+          if (quadrados[indice + 1] != 1) {
+            quadrados[indice + 1] = 2;
+            quadrados[indice] = 0;
+          }
+        }
+        return quadrados;
+      });
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,56 +1,20 @@
 import Quadrado from "@/components/quadrado"
 import lista from "@/data/lista";
 import { useEffect, useState } from "react";
+import atualizarArray from "./atualizarArray";
 
 
 export default function Home() {
-  
-  const quadrados = lista;
+
+  let quadrados = lista;
   const [modal, setModal] = useState(false)
 
   useEffect(() => {
-    document.body.addEventListener("keydown", (event) => {
-      // Tecla pressionada
-      let tecla = event.key;
-      console.log(tecla)
-      // Indice do vasco no tabuleiro
-      let indice = quadrados.indexOf(2);
-
-      // Andando para cima
-      if (tecla == 'w' || tecla == 'W' || tecla == "ArrowUp") {
-        if (indice - 23 >= 0) {
-          if (quadrados[indice - 23] != 1) {
-            quadrados[indice - 23] = 2;
-            quadrados[indice] = 0;
-          }
-        }
-        // Andando para a esquerda
-      } else if (tecla == 'a' || tecla == 'A' || tecla == "ArrowLeft") {
-        if (quadrados[indice - 1] != 1) {
-          quadrados[indice] = 0;
-          quadrados[indice - 1] = 2;
-        }
-        // Andando para baixo
-      } else if (tecla == 's' || tecla == 'S' || tecla == "ArrowDown") {
-        if (indice + 23 < 506) {
-          if (quadrados[indice + 23] != 1) {
-            quadrados[indice + 23] = 2;
-            quadrados[indice] = 0;
-          }
-        }
-        // Andando para a direita
-      } else if (tecla == 'd' || tecla == 'D' || tecla == "ArrowRight") {
-        if (quadrados[indice + 1] != 1) {
-          quadrados[indice + 1] = 2;
-          quadrados[indice] = 0;
-        }
-        console.log(lista)
-      }
-      // Vitória
-      if (quadrados[482] == 2) {
-        ganhou();
-      }
-    });
+    quadrados = atualizarArray()
+    // Vitória
+    if (quadrados[482] == 2) {
+      ganhou();
+    }
   })
 
   // Funcao que mosta o modal e toca a musica de vitoria
