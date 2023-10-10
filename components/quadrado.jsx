edeaccbaf50d17cd549a33498e04a8fc8d642d76
Jonatas Jackson Goncalves <jonatasg@weg.net>
2023-10-10 13:33:17 -0300

    correção dois pulos

--- components/quadrado.jsx
+++ components/quadrado.jsx
@@ -7,42 +7,7 @@ export default function Quadrado(props) {
     const quadrados = lista;
 
     useEffect(() => {
-        document.body.addEventListener("keydown", async (event) => {
-            let tecla = event.key;
-            let indice = quadrados.indexOf(2);
-
-            if (tecla == 'w' || tecla == 'W') {
-                if (indice - 23 >= 0) {
-                    if (quadrados[indice - 23] != 1) {
-                        quadrados[indice - 23] = 2;
-                        quadrados[indice] = 0;
-                    }
-                }
-            } else if (tecla == 'a' || tecla == 'A') {
-                if (quadrados[indice - 1] != 1) {
-                    quadrados[indice] = 0;
-                    quadrados[indice - 1] = 2;
-                }
-
-            } else if (tecla == 's' || tecla == 'S') {
-                if (indice + 23 < 506) {
-                    if (quadrados[indice + 23] != 1) {
-                        quadrados[indice + 23] = 2;
-                        quadrados[indice] = 0;
-                    }
-                }
-
-            } else if (tecla == 'd' || tecla == 'D') {
-                if (quadrados[indice + 1] != 1) {
-                    quadrados[indice + 1] = 2;
-                    quadrados[indice] = 0;
-                }
-                console.log(lista)
-            }
-            if (quadrados[482] == 2) {
-                console.log("oi 9vinha")
-            }
-
+        document.body.addEventListener("keydown", (event) => {
             if (lista[props.chave] == 2) {
                 setIsHere(true);
             } else {
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,8 +4,47 @@ import { useEffect, useState } from "react";
 
 
 export default function Home() {
-  let i=-1;
-  const quadrados = lista;
+  let i = -1;
+  const quadrados = lista; +
+
+    useEffect(() => {
+      document.body.addEventListener("keydown", (event) => {
+        let tecla = event.key;
+        let indice = quadrados.indexOf(2);
+
+        if (tecla == 'w' || tecla == 'W') {
+          if (indice - 23 >= 0) {
+            if (quadrados[indice - 23] != 1) {
+              quadrados[indice - 23] = 2;
+              quadrados[indice] = 0;
+            }
+          }
+        } else if (tecla == 'a' || tecla == 'A') {
+          if (quadrados[indice - 1] != 1) {
+            quadrados[indice] = 0;
+            quadrados[indice - 1] = 2;
+          }
+
+        } else if (tecla == 's' || tecla == 'S') {
+          if (indice + 23 < 506) {
+            if (quadrados[indice + 23] != 1) {
+              quadrados[indice + 23] = 2;
+              quadrados[indice] = 0;
+            }
+          }
+
+        } else if (tecla == 'd' || tecla == 'D') {
+          if (quadrados[indice + 1] != 1) {
+            quadrados[indice + 1] = 2;
+            quadrados[indice] = 0;
+          }
+          console.log(lista)
+        }
+        if (quadrados[482] == 2) {
+          console.log("oi 9vinha")
+        }
+      });
+    })
 
   return (
 
@@ -15,11 +54,11 @@ export default function Home() {
         {quadrados.map(quadrado => {
           i++;
           if (quadrado == 1) {
-            return <Quadrado parede key={i} isHere={false} chave={i}/>
+            return <Quadrado parede key={i} isHere={false} chave={i} />
           } else if (quadrado == 2) {
             return <Quadrado key={i} isHere chave={i} />
           }
-          return <Quadrado key={i} isHere={false} chave={i}/>
+          return <Quadrado key={i} isHere={false} chave={i} />
         })}
 
       </div>
