558baa40c6b1f5ccf74a05ba77f74835ef0a693f
Jonatas Jackson Goncalves <jonatasg@weg.net>
2023-10-10 13:38:43 -0300

    correção dois pulos

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   let i = -1;
-  const quadrados = lista; +
+  const quadrados = lista; 
 
     useEffect(() => {
       document.body.addEventListener("keydown", (event) => {
@@ -54,11 +54,11 @@ export default function Home() {
         {quadrados.map(quadrado => {
           i++;
           if (quadrado == 1) {
-            return <Quadrado parede key={i} isHere={false} chave={i} />
+            return <Quadrado parede key={i} chave={i} />
           } else if (quadrado == 2) {
             return <Quadrado key={i} isHere chave={i} />
           }
-          return <Quadrado key={i} isHere={false} chave={i} />
+          return <Quadrado key={i} chave={i} />
         })}
 
       </div>
